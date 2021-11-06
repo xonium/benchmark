a7a74a17b95dfc845c2bd48e73abad2e7206d486
Johan Öbrink <johan.erik.obrink@gmail.com>
2021-11-06 20:22:48 +0100

    🥴

--- Benchmark.Spa/ClientApp/src/InitApp.ts
+++ Benchmark.Spa/ClientApp/src/InitApp.ts
@@ -1,9 +1,14 @@
-import { browserHistory, HistoryAdapter } from "mobx-state-router";
+import { createBrowserHistory } from 'history';
+import { HistoryAdapter } from "mobx-state-router";
 import { RootStore } from "./Stores/RootStore";
 
 export const initApp = async () => {
   const rootStore = new RootStore();
   const { routerStore } = rootStore;
+
+  const browserHistory = createBrowserHistory({
+    basename: "/benchmark"
+});
 
   const historyAdapter = new HistoryAdapter(routerStore, browserHistory);
   historyAdapter.observeRouterStateChanges();
